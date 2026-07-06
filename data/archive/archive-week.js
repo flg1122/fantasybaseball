ca128b00c2cad69dff79be9cb72599a00ce3c1e3
flg1122 <flg@njit.edu>
2026-07-05 23:29:15 -0400

    archiving week

--- data/archive/archive-week.js
+++ data/archive/archive-week.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 
-const WEEK_NUMBER = 14;
+const WEEK_NUMBER = 15;
 
 
 const inputPath = path.join(__dirname, "..", "weekly-leaders.json");
